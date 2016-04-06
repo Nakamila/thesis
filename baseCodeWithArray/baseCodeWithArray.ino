ade30817b663805af6426dcdbecbf5ce00cc92d5
Camila Nakamura <gernc653@newschool.edu>
2016-04-06 16:46:53 -0400

    minorModifications

--- baseCodeWithArray/baseCodeWithArray.ino
+++ baseCodeWithArray/baseCodeWithArray.ino
@@ -19,10 +19,10 @@ boolean displayResult;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  //  Serial.println("Time (s)    Voltage Readings from A0 (N)");
+  Serial.println("Time (s)    Voltage Readings from A0 (N)");
 
-  readingSensor = true; // create a way to start/ stop reading the sensor
-  displayResult = false;// create a way to start/ stop displayng the lights
+  //  readingSensor = true; // create a way to start/ stop reading the sensor
+  //  displayResult = false;// create a way to start/ stop displayng the lights
 
 }
 
@@ -33,60 +33,29 @@ void loop() {
   float Time[rN];
   float Count[rN];
 
+  //
+  //  float t1 = 0;
+  //  float t2 = 0;
+  //  float sum = 0;
+  //  float count = 0;
+  //  float aux = 0;
 
-  float t1 = 0;
-  float t2 = 0;
-  float sum = 0;
-  float count = 0;
-  float aux = 0;
+  float reading = 0;
 
- 
-  //  Time = reading/1000 * TimeBetweenReadings;
-  //  Serial.println(Time);
+  for (int readIndex = 0; readIndex < 1500 ; readIndex++) {
 
-
-  for (int readIndex = 0; readIndex <= rN; readIndex++) {
-  
-    Time[readIndex] = (readIndex/1000.0*TimeBetweenReadings); //does the division first to avoid overflows
-    Serial.println(Time[readIndex]);
-    //Serial.print("\t");
+    Time[readIndex] = (reading / 1000.0 * TimeBetweenReadings); //does the division first to avoid overflows
+    Serial.print(Time[readIndex]);
+    Serial.print("\t");
 
     Count[readIndex] = analogRead(A0); // reads raw number
     delay(TimeBetweenReadings);// delay in between reads for stability
-    
+
     volt[readIndex] = Count[readIndex] / 1023 * 5.0;// convert from count to raw voltage
-    //Serial.println(volt[readIndex]);
+    Serial.println(volt[readIndex]);
+    reading++;
 
   }
 }
-
-
-
-
-    //    if (volt[readIndex] >= 4.6) { // using only the peaks moment
-    //      t2 = timer[readIndex];
-    //      aux = t2 - t1;  // find the present beat-to-beat interval
-    //      sum = aux + sum; // add the present beat-to-beat interval to the previous one
-    //      count = count++;   // find out how many times the PEAKS occured I have in 15 seconds to be able to calculate a media of beat-to-beat interval for that amount of time
-    //      t2 = t1;
-    //
-    //    }
-
-    //    if (readingNow >= rN) {
-    //      Serial.println("I AM DONE!");
-    //      //      delay(60000); // stop for a minute
-    //      //      readIndex = 0;
-    //    }
-
-// if we're at the end of the array...
-//  if (readIndex >= rN) {
-//    // .stop reading:
-//    readingSensor = false;
-//    displayResult = true;
-//    delay (60000) // display result for 1 minute
-//
-//  }
-
-
 
 
